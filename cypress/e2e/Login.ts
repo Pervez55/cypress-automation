e8b62a0f3490b35ab12d6dbeec6e64142c79d98e
pervezsoomro <105167500+pervezsoomro@users.noreply.github.com>
2023-05-14 00:50:52 +0200

    User Login Implementation

--- cypress/e2e/Login.ts
+++ cypress/e2e/Login.ts
@@ -2,12 +2,14 @@ export class LoginPage {
 
     loginPageLocators = {
         usernameField:() => cy.get("input[name='username']"),
-        passwordField:() => cy.get("input[name='password')]")
+        passwordField:() => cy.get("input[name='password']"),
+        loginBtn:() => cy.get("button[class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']")
     }
 
     enterUsernameAndPassword() {
         this.loginPageLocators.usernameField().type('Admin')
-
+        this.loginPageLocators.passwordField().type('admin123')
+        this.loginPageLocators.loginBtn().click()
         
     }
     
